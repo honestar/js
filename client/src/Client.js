6612b6762c30c358ca72f54a461c26f2a1708b8f
skyaie <skyaie@outlook.com>
2019-05-27 23:13:56 -0400

    Merge pull request #3 from gaearon/cors

--- client/src/Client.js
+++ client/src/Client.js
@@ -13,7 +13,6 @@ function checkStatus(response) {
     const error = new Error(`HTTP Error ${response.statusText}`);
     error.status = response.statusText;
     error.response = response;
-    console.log(error); // eslint-disable-line no-console
     throw error;
   }
 }
